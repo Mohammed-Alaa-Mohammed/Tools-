b691963e8d942b366871fd76e5590bdf42de4565
© Ŧie Kiɱ <111252375+DARKGITHUBPRO@users.noreply.github.com>
2024-11-04 21:23:57 +0300

    Add files via upload

---
+++ Banner Generator V.5.2.py
@@ -0,0 +1,296 @@
+# from pyperclip import *
+# import pyfiglet as ban
+# from pystyle import *
+# import platform
+# import os
+# import socket
+# import time
+# from tqdm import *
+# from colorama import Fore
+# import random
+# Banner of logo project
+# 🔴🟠🟡🟢🔵🟣
+from sympy.plotting.intervalmath import interval
+
+# Installer Library for start tool
+# checking....
+# 🇱🇴🇦🇩🇮🇳🇬…
+try:
+    from pyperclip import *
+    import pyfiglet as ban
+    from pystyle import *
+    import platform
+    import os
+    import socket
+    import time
+    from tqdm import tqdm , trange
+    from colorama import Fore
+    import random
+    import datetime
+
+except ModuleNotFoundError:
+    os.system('pip install pyfiglet')
+    os.system('pip install pyperclip')
+    os.system('pip install tqdm')
+    os.system('pip install pystyle')
+    os.system("pip install time")
+time.sleep(.020)
+
+
+print(Fore.LIGHTGREEN_EX,"""
+██████╗  █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ 
+██╔══██╗██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗
+██████╔╝███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
+██╔══██╗██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
+██████╔╝██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝     
+\33[91;1m                .---------------.
+                | [-] Generator |
+                '---------------'\33[39;0m       
+"""+'\33[94;1m\t [-] By : Developer Mohammed Alaa Mohammed\33[39;0m\n'+'\33[8;4m[-] V.1.02.4 is Last Update at \33[39;0m',f"\33[33;1m[-] {datetime.datetime.now()}\33[39;0m\n")
+
+
+print("""
+\33[93;1m            ╔─────── ☠ ☠ ☠ ───────╗\33[39;0m
+\33[96;1m𝟏- 𝐔𝐧𝐰𝐨𝐫𝐤 𝐓𝐡𝐢𝐬 𝐓𝐨𝐨𝐥 > 𝐖𝐫𝐢𝐭𝐞 : 𝐚𝐩𝐭 𝐮𝐩𝐝𝐚𝐭𝐞 && 𝐚𝐩𝐭 𝐮𝐩𝐠𝐫𝐚𝐝𝐞\33[39;0m
+\33[95;1m𝟐- 𝐅𝐨𝐥𝐥𝐨𝐰 𝐚𝐥𝐥 𝐒𝐭𝐞𝐩𝐬 𝐭𝐨 𝐞𝐱𝐢𝐭 𝐓𝐨𝐨𝐥\33[39;0m
+\33[93;1m𝟑- 𝐃𝐨𝐧'𝐭 𝐄𝐝𝐢𝐭 𝐭𝐡𝐢𝐬 𝐓𝐨𝐨𝐥\33[39;0m
+\33[92;1m𝟒- 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐆𝐢𝐭𝐡𝐮𝐛 : --⦸⦸..⦸⦸--\33[39;0m
+\33[8;1m𝟓- 𝐓𝐡𝐞 𝐓𝐡𝐢𝐬 𝐓𝐨𝐨𝐥 𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 𝐏𝐲𝐭𝐡𝐨𝐧 𝐕.𝟓.𝟐.2 🇱🇦🇸🇹 🇺🇩🇵🇩🇦🇹🇪...\33[39;0m
+\33[91;1m𝟔- 𝐀𝐥𝐥 𝐑𝐢𝐠𝐡𝐭𝐬 𝐑𝐞𝐬𝐞𝐫𝐯𝐞𝐝 𝐭𝐨 𝐭𝐡𝐞 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 :| \33[39;0m☠☠☠\33[032;01m 𝑴𝒐𝒉𝒂𝒎𝒎𝒆𝒅 𝑨𝒍𝒂𝒂 𝑴𝒐𝒉𝒂𝒎𝒎𝒆𝒅 \33[39;0m☠☠☠
+            \33[93;1m╚─────── ☠ ☠ ☠ ───────╝\33[39;0m
+""")
+
+time.sleep(.5)
+
+
+for i in tqdm(range(0, 100), ncols = 120,
+               desc='\33[39;0m\33[36;1m[\33[39;0m\33[91m-\33[96;1m]\33[39;0m Checking Updates..\33[39;0m',colour='White'):
+    time.sleep(.040)
+
+
+# Progress bar 2: Customized bar format and color
+for i in tqdm(range(100),ncols=120,colour='yellow',desc='\33[36;1m[\33[39;0m\33[91m-\33[96;1m]\33[39;0m Code Running Now... \33[39;0m'):
+    time.sleep(.020)
+
+
+    time.sleep(0.01)
+print("\n\33[39;0m[✅]\33[092;1m This is a Last Update Of This Tool , Tool size 12.1🇰🇧\33[39;0m [✅]")
+print("\33[39;0m[🚨️]\33[91;1m Enter English Only or any language in not arabic\33[39;0m [🚨]")
+print("\33[39;0m[🚨️]\33[91;1m If Not Install Library Wait a Download to Start Tool\33[39;0m [🚨]")
+print("\33[39;0m[🚨️]\33[091;1m This Tool Running of Linux - Windows System ONLY\33[39;0m [🚨]")
+print("\33[39;0m[💡]\33[092;1m This Tool Provides you with More than 10 Types of Fonts,Several Colors for The Shapes.\33[39;0m [💡]\n")
+
+
+                                                                    #============== END BANNER ============
+
+                                                                #========== SET INPUT to set name banner ============
+
+
+set_banner_name = input('\33[36;1m[\33[39;0m\33[91m-\33[96;1m]\33[39;0m\33[94;1m Enter Your Name to Create Banner : \33[39;0m')
+for i in tqdm(range(100),ncols=120,colour='red',desc='\33[36;1m[\33[39;0m\33[91m-\33[96;1m]\33[39;0m Step one..'):
+    time.sleep(0.040)
+print("﹌" * 20)
+Write.Print('[🟢] OK Continue to Follow Steps...\n',Colors.green,interval=0.030)
+print("﹌" * 20)
+font_style_list =(""" 
+\33[36;1m[\33[39;0m\33[91;1m01\33[96;1m]\33[39;0m >>> SLANT -\33[92;2m [Amazing]\33[39;0m\33[8;1m ✮✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m02\33[96;1m]\33[39;0m >>> 3-D -\33[33;1m [Normal]\33[39;0m \33[8;1m ✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m03\33[96;1m]\33[39;0m >>> 5LINEOBLIQUE -\33[33;1m [Normal]\33[39;0m \33[8;1m ✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m04\33[96;1m]\33[39;0m >>> ALPHABET -\33[33;1m [Normal]\33[39;0m \33[8;1m ✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m05\33[96;1m]\33[39;0m >>> DOH -\33[031;1m [Not Bad But It So Over Big Font]\33[39;0m \33[8;1m ✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m06\33[96;1m]\33[39;0m >>> ISOMETRIC1 -\33[33;1m [Normal]\33[39;0m \33[8;1m ✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m07\33[96;1m]\33[39;0m >>> BIG - \33[92;2m[Amazing]\33[39;0m \33[39;0m\33[8;1m ✮✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m08\33[96;1m]\33[39;0m >>> BANNER3 -\33[92;2m [Amazing]\33[39;0m \33[39;0m\33[8;1m ✮✮✮✮✮\33[39;0m 
+\33[36;1m[\33[39;0m\33[91;1m09\33[96;1m]\33[39;0m >>> BELL -\33[92;2m \33[33;1m[Normal]\33[39;0m \33[8;1m ✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m10\33[96;1m]\33[39;0m >>> CONTESSA -\33[92;2m \33[33;1m[Normal]\33[39;0m \33[8;1m ✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m11\33[96;1m]\33[39;0m >>> CYBERMEDIUM -\33[92;2m \33[91;1m[BAD]\33[39;0m \33[8;1m ✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m12\33[96;1m]\33[39;0m >>> DOOM -\33[92;2m \33[33;1m[Normal]\33[39;0m \33[8;1m ✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m13\33[96;1m]\33[39;0m >>> NTGREEK -\33[33;1m [Normal]\33[39;0m\33[8;1m ✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m14\33[96;1m]\33[39;0m >>> PUFFY -\33[92;2m [Amazing]\33[39;0m\33[8;1m ✮✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m15\33[96;1m]\33[39;0m >>> STARWARS -\33[92;2m \33[92;1m[TOP Style]\33[39;0m \33[8;1m ✮✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m16\33[96;1m]\33[39;0m >>> STOP -\33[92;2m [Amazing]\33[39;0m\33[8;1m ✮✮✮✮✮\33[39;0m
+\33[36;1m[\33[39;0m\33[91;1m17\33[96;1m]\33[39;0m >>> STANDARD -\33[92;2m \33[92;1m[TOP Style]\33[39;0m \33[8;1m ✮✮✮✮✮\33[39;0m   
+""")
+print(font_style_list)
+
+# Write.Print('[✅] OK Continue to Follow Steps...',Colors.green,interval=0.1)
+#=================================================================== CLOSE CHOOSE OF FONT style ====================================================================
+
+                                                                #========= START INPUT FOR USE OF choose style banner ==============
+print("﹌" * 20)
+set_font_style = input('\33[36;1m[\33[39;0m\33[91;1m➡️\33[96;1m]\33[39;0m\33[93;2m Enter \33[39;0m\33[93;4mNumber\33[39;0m\33[93;2m Of Font Style to Banner : \33[39;0m')
+
+for i in tqdm(range(100),ncols=120,colour='green',desc='\33[36;1m[\33[39;0m\33[91m➡️\33[96;1m]\33[39;0m Step two..',unit=f' You was Choose Number : '
+                                                                                                                         f'\33[93;1m({ set_font_style }) \33[39;0m'):
+    time.sleep(0.06000)
+print("﹌" * 20)
+
+#==================================
+
+Write.Print('[🟢] OK Continue to Follow Steps...\n',Colors.green,interval=0.030)
+print("﹌" * 20)
+
+                                                                #=============== LIST TO CHOOSE COLOR ===================
+font_color_list =(""" 
+\33[36;1m[\33[39;0m\33[91;1m1\33[96;1m]\33[39;0m >>> LIGHTGREEN_EX
+\33[36;1m[\33[39;0m\33[91;1m2\33[96;1m]\33[39;0m >>> LIGHTMAGENTA_EX
+\33[36;1m[\33[39;0m\33[91;1m3\33[96;1m]\33[39;0m >>> LIGHTYELLOW_EX
+\33[36;1m[\33[39;0m\33[91;1m4\33[96;1m]\33[39;0m >>> BLUE
+\33[36;1m[\33[39;0m\33[91;1m5\33[96;1m]\33[39;0m >>> RED
+\33[36;1m[\33[39;0m\33[91;1m6\33[96;1m]\33[39;0m >>> CYAN
+\33[36;1m[\33[39;0m\33[91;1m7\33[96;1m]\33[39;0m >>> GREEN
+\33[36;1m[\33[39;0m\33[91;1m8\33[96;1m]\33[39;0m >>> YELLOW
+\33[36;1m[\33[39;0m\33[91;1m9\33[96;1m]\33[39;0m >>> MAGENTA
+\33[36;1m[\33[39;0m\33[91;1m10\33[96;1m]\33[39;0m >>> WHITE
+\33[36;1m[\33[39;0m\33[91;1m11\33[96;1m]\33[39;0m >>> LIGHTRED_EX
+""")
+
+print(font_color_list)
+print("﹌" * 20)
+
+                                                        # ===============END LIST TO CHOOSE COLOR===================
+
+
+                                                            #=====SET INPUT to Choose COLOR=======
+
+set_font_color = input('\33[36;1m[\33[39;0m\33[91;1m*\33[96;1m]\33[39;0m\33[95;1m Enter Color Number for Banner : \33[39;0m')
+for i in tqdm(range(150),ncols=120,colour='blue',desc='\33[36;1m[\33[39;0m\33[91m*\33[96;1m]\33[39;0m Last Step ..',unit=f' You was Choose Number : '
+                                                                                                                    f'\33[93;1m ({ set_font_color }) \33[39;0m'):
+    time.sleep(.05000)
+
+
+# Write.Print("Note : If Choose any Number",Colors.yellow_to_red,interval=0)
+print("﹌" * 20)
+Write.Print('[✅] OK Continue to Follow Last Step ...\n', Colors.green, interval=0.01)
+print("﹌" * 20)
+
+
+# START >>> [ IF , ELIF , ELSE ] >>> TO CHOOSE OF USER
+lsit_number = ['1','2','3','4','5','6']
+
+if set_font_style == '1' :
+    banner = ban.figlet_format(set_banner_name,font='slant')
+
+elif set_font_style == '2':
+    banner = ban.figlet_format(set_banner_name, font='3-d')
+
+elif set_font_style == '3':
+    banner = ban.figlet_format(set_banner_name, font='5lineoblique')
+
+elif set_font_style == '4':
+    banner = ban.figlet_format(set_banner_name, font='alphabet')
+
+elif set_font_style == '5':
+    banner = ban.figlet_format(set_banner_name, font='doh')
+
+elif set_font_style == '6':
+    banner = ban.figlet_format(set_banner_name, font='isometric1')
+
+elif set_font_style == '7':
+    banner = ban.figlet_format(set_banner_name, font='big')
+
+elif set_font_style == '8':
+    banner = ban.figlet_format(set_banner_name, font='Banner3')
+
+
+elif set_font_style == '9':
+    banner = ban.figlet_format(set_banner_name, font='bell')
+
+elif set_font_style == '10':
+    banner = ban.figlet_format(set_banner_name, font='CONTESSA')
+
+elif set_font_style == '11':
+    banner = ban.figlet_format(set_banner_name, font='CYBERMEDIUM')
+
+elif set_font_style == '12':
+    banner = ban.figlet_format(set_banner_name, font='DOOM')
+
+elif set_font_style == '13':
+    banner = ban.figlet_format(set_banner_name, font='NTGREEK')
+
+elif set_font_style == '14':
+    banner = ban.figlet_format(set_banner_name, font='PUFFY')
+
+elif set_font_style == '15':
+    banner = ban.figlet_format(set_banner_name, font='STARWARS')
+
+elif set_font_style == '16':
+    banner = ban.figlet_format(set_banner_name, font="STOP")
+
+elif set_font_style == '17':
+    banner = ban.figlet_format(set_banner_name, font='STANDARD')
+
+elif set_font_style == '***':
+    quit()
+
+else:
+    print('\33[91;4m[❌] Error ID number not entered of Font Style \33[39;0m')
+
+
+                                                           #====== END INPUTS to Choose font , END IF - ELIF =======
+
+                                                            # ====== START END IF - ELIF TO CHOOSE COLOR =======
+
+
+
+if set_font_color == '1':
+    print(Fore.LIGHTGREEN_EX,banner)
+
+elif set_font_color == '2':
+    print(Fore.LIGHTMAGENTA_EX, banner)
+
+
+elif set_font_color == '3':
+    print(Fore.LIGHTYELLOW_EX, banner)
+
+elif set_font_color == '4':
+    print(Fore.BLUE, banner)
+
+
+elif set_font_color == '5':
+    print(Fore.RED, banner)
+
+elif set_font_color == '6':
+    print(Fore.CYAN, banner)
+
+
+elif set_font_color == '7':
+    print(Fore.GREEN, banner)
+
+elif set_font_color == '8':
+    print(Fore.YELLOW, banner)
+
+elif set_font_color == '9':
+    print(Fore.MAGENTA, banner)
+
+
+elif set_font_color == '10':
+    print(Fore.WHITE, banner)
+
+
+elif set_font_color == '11':
+    print(Fore.LIGHTRED_EX, banner)
+
+else:
+    print('\n\33[91;4m[❌] Error ID number not entered Of Color \n\33[39;0m')
+
+print("\33[90;3m﹌\33[39;0m" * 25)
+
+Write.Print('You Can Copy This Your Banner Now , and You Can Edit it Banner\n',Colors.light_red,interval=0.02)
+
+print("\33[090;2m﹌\33[39;0m" * 25)
+
+user_aked = Write.Input('Do You Want Copy Your Text Banner.. (n/y) : ',Colors.yellow,interval=0).lower()
+if user_aked == 'y':
+     copy(banner+set_banner_name+set_font_style+set_font_color)
+     print('\n\33[92;1mBanner Was Copy in Your clipboard for open this (CTRL + V) \33[39;0m')
+
+
+
+
+
+
+
+
+# By Developer ::| MOHAMMED ALAA MOHAMMED
